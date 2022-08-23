82a78a935ce32468d5cbad84c5d11c62b1ec235f
Jonathas Sodré <jonathas_sl@hotmail.com>
2022-08-23 15:34:38 -0300

    criação da estrutura do menu

--- Secao01/ProjetoCurso/Program.cs
+++ Secao01/ProjetoCurso/Program.cs
@@ -10,13 +10,55 @@ namespace ProjetoCurso
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Selecione a opção abaixo:");
-            Console.WriteLine("1 - Cadastrar Cliente");
-            Console.WriteLine("2 - Listar Clientes");
-            Console.WriteLine("3 - Cadastrar Funcionário");
-            Console.WriteLine("4 - Listar Funcionários");
+            Tela.ManterCliente manterCliente = new Tela.ManterCliente();
+            Tela.ManterFuncionario manterFuncionario = new Tela.ManterFuncionario();
+            int opcoes = 0;
 
-            Console.ReadKey();
+            try
+            {
+
+                while (opcoes != 5)
+                {
+
+
+                    Console.WriteLine("Selecione a opção abaixo:");
+                    Console.WriteLine("1 - Cadastrar Cliente");
+                    Console.WriteLine("2 - Listar Clientes");
+                    Console.WriteLine("3 - Cadastrar Funcionário");
+                    Console.WriteLine("4 - Listar Funcionários");
+                    Console.WriteLine("5 - Sair");
+
+                    opcoes = int.Parse(Console.ReadLine());
+
+                    switch (opcoes)
+                    {
+                        case 1:
+                            manterCliente.CadastrarCliente();
+                            break;
+                        case 2:
+                            manterCliente.ListarClientes();
+                            break;
+                        case 3:
+                            manterFuncionario.CadastrarFuncionario();
+                            break;
+                        case 4:
+                            manterFuncionario.ListarFuncionarios();
+                            break;
+                        case 5:
+                            Console.WriteLine("Obrigado.");
+                            break;
+                        default:
+                            Console.WriteLine("Opção Inválida");
+                            break;
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Informar apenas números no menu!");
+            }
+       
         }
-    }
-}
+     }
+ }
---
+++ Secao01/ProjetoCurso/Tela/ManterFuncionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCurso.Tela
+{
+    public class ManterFuncionario
+    {
+        public void CadastrarFuncionario()
+        {
+
+        }
+
+        public void ListarFuncionarios()
+        {
+
+        }
+    }
+}
