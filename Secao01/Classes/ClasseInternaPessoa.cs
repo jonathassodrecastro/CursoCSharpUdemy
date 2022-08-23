0e59f4a0624da49007212f7bdf262a4f67288b05
Jonathas Sodré <jonathas_sl@hotmail.com>
2022-08-23 14:59:17 -0300

    Classes

---
+++ Secao01/Classes/ClasseInternaPessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class ClasseInternaPessoa
+    {
+        void GetOlhos()
+        {
+            //instanciando a classe olho dentro da classe pessoa
+            Olho olho = new Olho();
+        }
+        
+        //com o modificador public a classe olho passa a ser acessível pelo programa
+        public class Olho
+        {
+            //classe interna criada com a mesma sintaxe mas dentro de uma classe
+            //a classe olho só é acessível pela classe pessoa
+            
+            //instanciando pessoa dentro de olho
+            public Olho()
+            {
+                ClasseInternaPessoa pessoa = new ClasseInternaPessoa();
+            }
+           
+            public static string Iris = "Castanho";
+
+           
+        }
+    }
+}
---
+++ Secao01/Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //para instanciar Olho no Main eu preciso chamar a ClasseInternaPessoa
+            new ClasseInternaPessoa.Olho();
+
+            string iris = ClasseInternaPessoa.Olho.Iris;
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+    }
+}
+
