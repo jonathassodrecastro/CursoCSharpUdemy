215b092d43e5ea746d3019026a76d6a7b96fea43
Jonathas Sodré <jonathas_sl@hotmail.com>
2022-08-23 14:22:15 -0300

    passagem de parametro por valor, ref e out

---
+++ Secao01/03_Estruturas/Polegada.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Estruturas
+{
+    struct Polegada
+    {
+        double Centimetro;
+        double polegada;
+
+        public Polegada(double polegadas)
+        {
+            Centimetro = 2.54;
+            polegada = Centimetro * polegadas;
+        }
+
+        public double GetPolegadaCentimetro() { return polegada; }
+    }
+}
---
+++ Secao01/04_PassagemParametroPorValor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_PassagemParametro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 25;
+            Console.WriteLine("Main > var valor: " + valor);
+
+            AlterarValor(valor);
+            Console.WriteLine("Main > var valor: " + valor);
+
+            AlterarValorPorRef(ref valor);
+            Console.WriteLine("Main > var valor: " + valor);
+
+            AlterarValorOut(out valor);
+            Console.WriteLine("Main > var valor: " + valor);
+
+            Console.ReadKey();
+        }
+        static void AlterarValor(double valor)
+        { 
+            Console.WriteLine("AlterarValor > var valor: " + valor);
+
+            valor += 10;
+
+            Console.WriteLine("AlterarValor > var valor: " + valor);
+        }
+
+        static void AlterarValorPorRef (ref double valor)
+        {
+            Console.WriteLine("AlterarValorPorRef > var valor: " + valor);
+
+            valor += 15;
+
+            Console.WriteLine("AlterarValorPorRef > var valor: " + valor);
+        }
+
+        static void AlterarValorOut(out double valor) 
+        {
+
+            valor = 15;
+
+            Console.WriteLine("AlterarValorOut > var valor: " + valor);
+        }
+    }
+}
